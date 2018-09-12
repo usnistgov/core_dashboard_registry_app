5661d725441d1b11315e1d0cec7530e5271976cd
Xavier SCHMITT <xavier.schmitt@nist.gov>
2018-09-12 11:05:57 -0400

    fix: remove unused constant
    Change-Id: I59acaa16e08c1fefff3b84507e7ad03cd7fc8892

--- core_dashboard_registry_app/settings.py
+++ core_dashboard_registry_app/settings.py
@@ -9,7 +9,3 @@ if not settings.configured:
 SERVER_URI = getattr(settings, 'SERVER_URI', "http://localhost")
 
 INSTALLED_APPS = getattr(settings, 'INSTALLED_APPS', [])
-
-DATA_DISPLAY_NAME = getattr(settings, 'DATA_DISPLAY_NAME', 'resource')
-DRAFT_DISPLAY_NAME = getattr(settings, 'DRAFT_DISPLAY_NAME', 'draft')
-WORKSPACE_DISPLAY_NAME = getattr(settings, 'WORKSPACE_DISPLAY_NAME', 'workspace')
