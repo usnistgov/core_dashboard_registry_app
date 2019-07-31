c6db78b02f0865e52955295ff29e2898798d3eeb
Xavier SCHMITT <xavier.schmitt@nist.gov>
2019-07-31 10:10:04 -0400

    fix: unselect icon
    Change-Id: Id07e5d4e68deb782c0104222065dd0c3bc46fe28

--- core_dashboard_registry_app/static/core_dashboard_registry_app/user/js/record_registry.js
+++ core_dashboard_registry_app/static/core_dashboard_registry_app/user/js/record_registry.js
@@ -37,16 +37,20 @@ is_all_td_selected = function() {
  */
 click_role = function(role) {
 
-    $("#td_" + role).addClass("selected_resource");
-    if (role != role_custom_resource_type_all) {
-         $("#td_"+ role_custom_resource_type_all).removeClass('selected_resource')
-    }
-    if (role == role_custom_resource_type_all || is_all_td_selected()) {
-        var list_roles = list_role_custom_resource.split(',');
-        for (var i = 0 ; i<list_roles.length; i++) {
-            $("#td_" + list_roles[i]).removeClass('selected_resource');
+    if ($("#td_" + role).hasClass("selected_resource")) {
+        $("#td_" + role).removeClass("selected_resource");
+    } else {
+        $("#td_" + role).addClass("selected_resource");
+        if (role != role_custom_resource_type_all) {
+             $("#td_"+ role_custom_resource_type_all).removeClass('selected_resource')
         }
-        $("#td_"+ role_custom_resource_type_all).addClass('selected_resource')
+        if (role == role_custom_resource_type_all || is_all_td_selected()) {
+            var list_roles = list_role_custom_resource.split(',');
+            for (var i = 0 ; i<list_roles.length; i++) {
+                $("#td_" + list_roles[i]).removeClass('selected_resource');
+            }
+            $("#td_"+ role_custom_resource_type_all).addClass('selected_resource')
+        }
     }
 };
 
